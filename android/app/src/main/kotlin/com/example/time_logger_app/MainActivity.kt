79ab000a089b1391f52fbd2fbfbdb2497105a30f
tigerrad <tigerrad@users.noreply.github.com>
2026-09-20 13:37:54 +0330

    Initial commit - Time Logger App

---
+++ android/app/src/main/kotlin/com/example/time_logger_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.time_logger_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+﻿import 'package:flutter_test/flutter_test.dart';
+import 'package:time_logger_app/main.dart';
+
+void main() {
+  testWidgets('App loads', (WidgetTester tester) async {
+    await tester.pumpWidget(const TimeLoggerApp());
+    expect(find.text('تایم لاگر — سازنده: کورش شیراز'), findsOneWidget);
+  });
+}
